a93446b88fd8fdcb01717f32117d54af5d141e66
bbhtest <98625691+bbhtest@users.noreply.github.com>
2022-01-29 08:46:50 +0800

    test1

---
+++ js/slick.js
@@ -0,0 +1,11 @@
+(function($) {
+  $(document).ready(function() {
+      $('.photo').slick({
+          dots: true,
+          infinite: true,
+          cssEase: 'linear',
+          swipe: false,
+      });
+  });
+
+})( jQuery );
